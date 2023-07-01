a4dea3d2c2680d942ce4b249f53e3c7062702d9c
artemsaltykov <138072196+artemsaltykov@users.noreply.github.com>
2023-07-01 13:00:21 +0400

    Сделан переход на страницу админа после авторизации

---
+++ login.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "admin" && password === "admin123") {
+        window.location = 'http://localhost/site/adminpage.php';
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
